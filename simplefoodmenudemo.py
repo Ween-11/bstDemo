02f9d73794827c6216b04c950af367c72684ce9c
Ween-11 <89080901+Ween-11@users.noreply.github.com>
2021-10-26 17:05:00 +0800

    Update simplefoodmenudemo.py

--- simplefoodmenudemo.py
+++ simplefoodmenudemo.py
@@ -18,7 +18,7 @@ if readme:
 
     st.write ("For more info, please contact:")
 
-    st.write("<a href='weenmae@gmail.com'>Sharween </a>", unsafe_allow_html=True)
+    st.write("<a href='weenmae@gmail.com'>Sharween Ismail</a>", unsafe_allow_html=True)
 
 st.write("Choose an option from the drop down on the side bar to continue.")
 
